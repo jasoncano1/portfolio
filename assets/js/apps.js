097a5bcbd74ac44db6e2730f9cdf460fc783e840
[Jason Cano] <jasoncano0509@gmail.com>
2023-09-22 10:05:28 -0500

    added header bs=ackground, played with css styling.

---
+++ assets/js/apps.js
@@ -0,0 +1,39 @@
+const apps = [
+    {
+        name: 'PicQuest',
+        url: 'https://shielded-spire-14458-df45cd7a37c0.herokuapp.com/',
+        description: 'The "PicQuest" application was created by a team of developers. It allows uploads and sharing of professional photos.',
+        github: 'https://github.com/jasoncano1/PicQuest',
+        image: 'assets/images/picquest.png',
+    },
+    {
+        name: 'Note Taker',
+        url: 'https://notetaker-jc-7e9b2e3fe567.herokuapp.com/',
+        description: 'This is a Heroku hosted application that allows you to create, save, and delete notes.',
+        github: 'https://github.com/jasoncano1/notetaker',
+        image: 'assets/images/.gif'
+    },
+    {
+        name: 'Quiz-Game',
+        url: 'https://jasoncano1.github.io/quiz-game/',
+        description: 'A 5 question quiz style game for javascript questions.',
+        github: 'https://github.com/jasoncano1/quiz-game',
+        image: 'assets/images/quiz-game.gif',
+    },
+    {
+        name: 'Weater Dashboard',
+        url: 'https://jasoncano1.github.io/weather-dashboard',
+        description: 'A 5 day forecaster that will also give current weather values for your city search.',
+        github: 'https://github.com/jasoncano1/weather-dashboard',
+        image: 'assets/images/weather.png',
+    },
+    {
+        name: 'MVC Tech Blog',
+        url: 'https://mvc-tech-blog-jc-79d395df94ac.herokuapp.com/',
+        description: 'This is my portfolio website. It is a single page application built with Vue.js and Tailwind CSS.',
+        github: 'https://github.com/jasoncano1/mvc-tech-blog',
+        image: 'assets/images/mvc.gif'
+    }
+]
+
+    
