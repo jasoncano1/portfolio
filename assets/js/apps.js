3103fa3e1b152a7181fcdd9e16b9c787a299d277
[Jason Cano] <jasoncano0509@gmail.com>
2023-10-06 09:36:23 -0500

    Added Creation of AAdam, finished work animation with the cards, and more css styling.

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,21 +1,21 @@
 const apps = [
     {
         name: 'PicQuest',
-        url: 'https://shielded-spire-14458-df45cd7a37c0.herokuapp.com/',
+        url: 'https://shielded-spire-14458-df45cd7a37c0.herokuapp.com',
         description: 'The "PicQuest" application was created by a team of developers. It allows uploads and sharing of professional photos.',
         github: 'https://github.com/jasoncano1/PicQuest',
         image: 'assets/images/picquest.png',
     },
     {
         name: 'Note Taker',
-        url: 'https://notetaker-jc-7e9b2e3fe567.herokuapp.com/',
+        url: 'https://notetaker-jc-7e9b2e3fe567.herokuapp.com',
         description: 'This is a Heroku hosted application that allows you to create, save, and delete notes.',
         github: 'https://github.com/jasoncano1/notetaker',
         image: 'assets/images/note-taker.png'
     },
     {
         name: 'Quiz-Game',
-        url: 'https://jasoncano1.github.io/quiz-game/',
+        url: 'https://jasoncano1.github.io/quiz-game',
         description: 'A 5 question quiz style game for javascript questions.',
         github: 'https://github.com/jasoncano1/quiz-game',
         image: 'assets/images/quiz-game.gif',
@@ -29,25 +29,33 @@ const apps = [
     },
     {
         name: 'MVC Tech Blog',
-        url: 'https://mvc-tech-blog-jc-79d395df94ac.herokuapp.com/',
+        url: 'https://mvc-tech-blog-jc-79d395df94ac.herokuapp.com',
         description: 'This is my portfolio website. It is a single page application built with Vue.js and Tailwind CSS.',
         github: 'https://github.com/jasoncano1/mvc-tech-blog',
         image: 'assets/images/mvc.gif'
     }
 ]
 
-document.querySelector('.apps').innerHTML = '';
-
-apps.forEach(app => {
-    document.querySelector('.apps').innerHTML += `
-    <a href="${app.url}">
-          <div class="app">
-            <img src="${app.image}" alt="Application">
-            <div class="info">
-              <h3>${app.name}</h3>
-              <p>${app.description}</p>
-            </div>
-          </div>
-        </a>`
+apps.forEach((app, i) => {
+    document.querySelector('#appNav').innerHTML += `
+            <img onclick="getApp(${i})" src="${app.image}" alt="Application">`
 });
+
+const getApp = i => {
+    a1.href = apps[i].url;
+    a2.href = apps[i].url;
+    a3.href = apps[i].github;
+    h1.innerHTML = apps[i].name;
+    info.style.animation = 'unset';
+    img1.style.animation = 'unset';
+    p1.innerHTML = apps[i].description;
+    setTimeout(() => img1.src = apps[i].image, 600);
+    setTimeout(()=>info.style.animation = 'activeInfo 1s linear',100);
+    setTimeout(() => img1.style.animation = 'activeApp 1s linear', 100);
+}
+
+getApp(0)
+
+const mobileResponse = () => {
     
+}
